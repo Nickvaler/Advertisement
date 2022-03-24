97d800ce4be81153f35507ad01fcbcdf2cde1f71
Nickvaler <valsalery@gmail.com>
2022-03-24 12:03:08 +0300

    delete models project

--- Advertisement.Models/DbModels/AdTags.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Advertisement.Models.DbModels
-{
-    public class AdTags
-    {
-        public int Id { get; set; }
-        public int IdsId { get; set; }
-        public int TagsId { get; set; }
-    }
-}
